24bbffc3cc0c41e8c392ccc8a048942771f60891
push-ebx <nikita.keyn@yandex.ru>
2023-04-18 22:33:30 +0300

    added loader for article

--- client/src/test/index.tsx
+++ client/src/test/index.tsx
@@ -4,9 +4,12 @@ import CustomMarkdown from "entities/customMarkdown"
 import { storage } from "shared/config/firebase";
 import { ref, getDownloadURL } from "firebase/storage";
 import axios from "axios";
+import { Loader } from "shared/ui/loader";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Test() {
-  const [value, setValue] = React.useState('# 123');
+  const [value, setValue] = React.useState(null);
   const imagesListRef = ref(storage, "articles/testArticle.md");
   
   useEffect(() => {
@@ -18,7 +21,8 @@ export default function Test() {
 
   return (
     <div style={{background: "var(--background-page-color)", transition: 'background 0.4s ease', height: "100vh"}}>
-      <div className="container" style={{display: 'flex', columnGap: 20}}>
+      { value ? 
+      (<div className="container" style={{display: 'flex', columnGap: 20}}>
         {/* <MDEditor
           value={value}
           //@ts-ignore
@@ -41,13 +45,14 @@ export default function Test() {
             background: "var(--background-article-color)", 
             borderRadius: 5
         }}>
-          <CustomMarkdown
-            style={{}}
-          >
+          <CustomMarkdown>
             {value}
           </CustomMarkdown>
         </div>
-      </div>
+      </div>)
+      : (
+        <Loader style={{position: 'absolute', top: '50%', left: '50%'}}/>
+      )}
     </div>
   );
 }
