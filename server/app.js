1ad5b84036658aceb2adc87ab90994f62dc5ca73
push-ebx <nikita.keyn@yandex.ru>
2023-04-24 23:36:28 +0300

    experiments

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = require("./router")
 
 const app = express();
-const PORT = 4000
+const PORT = 80
 
 const {firebaseConfig} = require("./firebaseConfig")
 console.log(firebaseConfig)
